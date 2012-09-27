160a07b05c2d3c96051ad8602b1a2d882debd27c
Marcin Kurzyna <m.kurzyna@crystalpoint.pl>
2012-09-28 00:21:18 +0200

    JavaScript toWords library
    - converts numbers to words
    - provides angular filters ($locale aware)

---
+++ dicts/towords.pl.js
@@ -0,0 +1,132 @@
+/**
+ *  Copyright (C) 2012 Marcin Kurzyna
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
+(function polishToWordsDictionaryDefiner() {
+    var words = {};
+
+    if (typeof toWords === 'undefined')
+        return;
+
+    words.digits = [
+        '', 'jeden', 'dwa', 'trzy', 'cztery', 'pięć', 'sześć', 'siedem', 'osiem', 'dziewięć' ,
+        'dziesięc', 'jedenaście', 'dwanascie', 'trzynaście', 'czternaście', 'piętanście', 'szesnaście',
+        'siedemnaście', 'osiemnaście', 'dziewiętnaście'
+    ];
+
+    words.tens = [
+        '', '', 'dwadzieścia', 'trzydzieści', 'czterdzieści', 'pięćdziesiąt', 'sześćdziesiąt', 'siedemdziesiąt',
+        'osiemdziesiąt', 'dziesięćdziesiąt'
+    ];
+
+    words.hundrets = [
+        '', 'sto', 'dwieście', 'trzysta', 'czterysta', 'pięćset', 'sześćset', 'siedemset',
+        'osiemset', 'dziewięćset'
+    ];
+
+    words.groups = [
+        ['','',''],
+        ['tysiąc','tysiące','tysięcy'],
+        ['milion','miliony','milionów'],
+        ['miliard','miliardy','miliardów'],
+        ['bilion','biliony','bilionów'],
+        ['biliard','biliardy','biliardów'],
+        ['trylion','tryliony','trylionów'],
+        ['tryliard','tryliardy','tryliardów'],
+        ['kwadrylion','kwadryliony','kwadrylionów'],
+        ['kwadryliard','kwadryliardy','kwadryliardów'],
+        ['kwintylion','kwintyliony','kwintylionów'],
+        ['kwintyliiard','kwintyliardy','kwintyliardów'],
+        ['sekstylion','sekstyliony','sekstylionów'],
+        ['sekstyliard','sekstyliardy','sekstyliardów'],
+        ['septylion','septyliony','septylionów'],
+        ['septyliard','septyliardy','septyliardów'],
+        ['oktylion','oktyliony','oktylionów'],
+        ['oktyliard','oktyliardy','oktyliardów'],
+        ['nonylion','nonyliony','nonylionów'],
+        ['nonyliard','nonyliardy','nonyliardów'],
+        ['decylion','decyliony','decylionów'],
+        ['decyliard','decyliardy','decyliardów'],
+        ['centylion','centyliony','centylionów'],
+        ['centyliard','centyliardy','centyliardów'],
+        ['wicylion','wicylion','wicylion'],
+        ['wicyliard','wicyliardy','wicyliardów'],
+        ['trycylion','trycylion','trycylion'],
+        ['trycyliard','trycyliardy','trycyliardów'],
+        ['kwadragilion','kwadragilion','kwadragilion'],
+        ['kwadragiliard','kwadragiliardy','kwadragiliardów'],
+        ['kwinkwagilion','kwinkwagilion','kwinkwagilion'],
+        ['kwinkwagiliard','kwinkwagiliardy','kwinkwagiliardów'],
+        ['seskwilion','seskwilion','seskwilion'],
+        ['seskwiliard','seskwiliardy','seskwiliardów'],
+        ['septagilion','septagilion','septagilion'],
+        ['septagiliard','septagiliardy','septagiliardów'],
+        ['oktogilion','oktogilion','oktogilion'],
+        ['oktogiliard','oktogiliardy','oktogiliardów'],
+        ['nonagilion','nonagilion','nonagilion'],
+        ['nonagiliard','nonagiliardy','nonagiliardów'],
+        ['centylion','centyliony','centylionów'],
+        ['centyliard','centyliardy','centyliardów']
+    ];
+
+    toWords.setDict(function toWordsDictionaryPL(number) {
+        var s = 0, d = 0, j = 0, k = 0, g = 0,
+            result = '';
+
+        if (number === 0) {
+            return 'zero';
+        }
+
+        if (number < 0) {
+            result += 'minus ';
+        }
+
+        while (Math.abs(number) > 0) {
+
+            s = Math.floor((number % 1000)/100),
+            d = Math.floor((number % 100)/10),
+            j = Math.floor(number % (d==1?20:10));
+
+            if (d === 0 && j === 1) {
+                k = 0; // "tysiąc"
+            } else if ( j > 1 && j < 5) {
+                k = 1; // "tysiące"
+            } else {
+                k = 2; // "tysięcy"
+            }
+
+            // exists at least one...
+            if (s+d+j > 0) {
+                result =
+                    words.hundrets[s] + ' ' +
+                    words.tens[d] + ' ' +
+                    words.digits[j] + ' ' +
+                    words.groups[g][k] + ' ' +
+                    result;
+            }
+
+            g++;
+            number = Math.floor(number/1000);
+        }
+
+        return result;
+
+    }, 'pl');
+})();
---
+++ towords.js
@@ -0,0 +1,77 @@
+/**
+ *  Copyright (C) 2012 Marcin Kurzyna
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ *
+ *  Usage:
+ *
+ *  var wordString = new toWords(number, locale).translate();
+ *
+ *  Example:
+ *
+ *  var wordString =
+ *      new toWords(11).
+ *      translate(); // "jedenaście"
+ *
+ *  var wordString =
+ *      new toWords(11, 'es').
+ *      setDict(toWordsDictionaryES).
+ *      translate(); // "once"
+ *
+ */
+var toWords = (function () {
+    var toWordsClass, dicts = {};
+
+    // main object definition
+    toWordsClass = function toWordsConstructor(number, locale) {
+        this.locale = locale || 'pl';
+        this.number = parseInt(number, 10);
+
+        if (isNaN(this.number)) {
+            throw new Error('Argument is not a number');
+        }
+    };
+
+    // static dictionary setter
+    toWordsClass.setDict = function(dict, locale) {
+        if (typeof dict !== 'function') {
+            throw new Error('Dictionary must be a function');
+        }
+
+        dicts[locale] = dict;
+    };
+
+    toWordsClass.prototype.setDict = function(dict, locale) {
+        toWordsClass.setDict(dict, locale || this.locale);
+        return this;
+    };
+
+    toWordsClass.prototype.translate = function() {
+        var me = this;
+
+        if (typeof dicts[this.locale] === 'undefined') {
+            throw new Error('Dictionary for this locale is not set');
+        }
+
+        return dicts[this.locale].call(me, parseInt(this.number, 10));
+    };
+
+    return toWordsClass;
+})();
+
